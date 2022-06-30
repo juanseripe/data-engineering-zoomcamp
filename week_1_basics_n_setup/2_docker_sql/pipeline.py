713712f4e1fdb8108e4f5b73252ab08671cf02af
JuanSe <juanseripe@gmail.com>
2022-06-30 00:45:15 -0600

    Week 1

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -1,3 +1,5 @@
+#docker run -it  -e POSTGRES_USER="root" -e POSTGRES_PASSWORD="root" -e POSTGRES_DB="ny_taxi" -v ny_taxi_postgres_data:/var/lib/postgresql/data -p 5432:5432 postgres:13
+
 import sys
 
 import pandas as pd
